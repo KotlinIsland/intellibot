b46e7e2d13133141dde48fc5c923024b0101857b
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-04-24 20:46:03 +1000

    RobotFileTypeHandler -> FileType

--- src/main/java/com/millennialmedia/intellibot/psi/RobotFileTypeHandler.java
+++ src/main/java/com/millennialmedia/intellibot/psi/RobotFileTypeHandler.java
@@ -1,16 +1,42 @@
 package com.millennialmedia.intellibot.psi;
 
-import com.intellij.openapi.fileTypes.FileTypeConsumer;
-import com.intellij.openapi.fileTypes.FileTypeFactory;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.util.NlsSafe;
+import com.millennialmedia.intellibot.ide.icons.RobotIcons;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
 
 /**
  * @author mrubino
  */
-public class RobotFileTypeHandler extends FileTypeFactory {
+public class RobotFileTypeHandler implements FileType {
 
     @Override
-    public void createFileTypes(@NotNull FileTypeConsumer consumer) {
-        consumer.consume(RobotFeatureFileType.getInstance());
+    public @NonNls @NotNull String getName() {
+        return "RobotFramework";
+    }
+
+    @Override
+    public @NlsContexts.Label @NotNull String getDescription() {
+        return "Feature file for robot framework language";
+    }
+
+    @Override
+    public @NlsSafe @NotNull String getDefaultExtension() {
+        return "robot";
+    }
+
+    @Override
+    public @Nullable Icon getIcon() {
+        return RobotIcons.FILE;
+    }
+
+    @Override
+    public boolean isBinary() {
+        return false;
     }
 }
