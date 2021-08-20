c2d9edaa0cfd8d528d3022538030ca9ef0d42abb
ngn2000 <ngn2000@126.com>
2021-08-20 19:18:55 +0800

    apply patch jamofer@962c570 to support Tasks

--- src/com/millennialmedia/intellibot/psi/element/HeadingImpl.java
+++ src/com/millennialmedia/intellibot/psi/element/HeadingImpl.java
@@ -74,7 +74,7 @@ public class HeadingImpl extends RobotPsiElementBase implements Heading {
     public boolean containsTestCases() {
         // TODO: better OO
         String text = getPresentableText();
-        return text.startsWith("*** Test Case");
+        return text.matches("^\\*\\*\\* (Test Case|Task).*");
     }
 
     @Override
