79e460d1ac48d62d4b166809ea26ca756fa585a1
KotlinIsland <kotlinisland@users.noreply.github.com>
2023-03-08 09:55:22 +1000

    upgradle: kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,25 @@
 plugins {
-    java
-    id("org.jetbrains.intellij") version "1.6.0"
+    kotlin("jvm") version "1.8.20-Beta"
+    id("org.jetbrains.intellij") version "1.13.1"
 }
 
 repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies{
     implementation("org.jetbrains:annotations:23.0.0")
 }
 
-val intellijVersion = System.getenv().getOrDefault("IDEA_VERSION", "PC-2022.1")
+val intellijVersion = System.getenv().getOrDefault("IDEA_VERSION", "PC-2022.3.2")
 
 val pythonPluginForVersion = mapOf(
-    "PC-2022.1" to "python-ce",
-    "IC-2022.1" to "PythonCore:221.5744.248",
+    "PC-2022.3.2" to "python-ce",
+    "IC-2023.1" to "PythonCore:231.5744.248",
 )
 
 intellij {
@@ -23,8 +27,9 @@ intellij {
     plugins.set(listOf(pythonPluginForVersion[intellijVersion]))
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 tasks.patchPluginXml {
+    sinceBuild.set("222")
     untilBuild.set("999.*")
 }
