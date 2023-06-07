b02df515a23913e7d81569e4612a86915310eeec
KotlinIsland <kotlinisland@users.noreply.github.com>
2023-06-07 13:21:40 +1000

    updoot id and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.20-Beta"
-    id("org.jetbrains.intellij") version "1.13.1"
+    kotlin("jvm") version "1.9.0-Beta"
+    id("org.jetbrains.intellij") version "1.13.2"
 }
 
 repositories {
@@ -15,10 +15,10 @@ dependencies{
     implementation("org.jetbrains:annotations:23.0.0")
 }
 
-val intellijVersion = System.getenv().getOrDefault("IDEA_VERSION", "PC-2022.3.2")
+val intellijVersion = System.getenv().getOrDefault("IDEA_VERSION", "PC-2023.1.2")
 
 val pythonPluginForVersion = mapOf(
-    "PC-2022.3.2" to "python-ce",
+    "PC-2023.1.2" to "python-ce",
     "IC-2023.1" to "PythonCore:231.5744.248",
 )
 
